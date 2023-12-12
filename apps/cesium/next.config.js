c5585dc32d629888bdc820534ae8d56a75f32a4e
Shota Matsuda <shota@takram.com>
2023-12-13 05:18:26 +0900

    Use copy plugin to serve cesium assets

--- apps/cesium/next.config.js
+++ apps/cesium/next.config.js
@@ -2,6 +2,8 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -16,6 +18,37 @@ const nextConfig = {
   compiler: {
     // For other options, see https://nextjs.org/docs/architecture/nextjs-compiler#emotion
     emotion: true
+  },
+
+  webpack: config => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(
+              __dirname,
+              '../../node_modules/@cesium/engine/Source/Assets'
+            ),
+            to: path.join(__dirname, 'public/cesium/Assets')
+          },
+          {
+            from: path.join(
+              __dirname,
+              '../../node_modules/@cesium/engine/Build/ThirdParty'
+            ),
+            to: path.join(__dirname, 'public/cesium/ThirdParty')
+          },
+          {
+            from: path.join(
+              __dirname,
+              '../../node_modules/@cesium/engine/Build/Workers'
+            ),
+            to: path.join(__dirname, 'public/cesium/Workers')
+          }
+        ]
+      })
+    )
+    return config
   }
 }
 
